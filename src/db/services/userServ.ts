9a6bea865f86849da958585139463b72a6e1ed05
David Hordiienko <neonz96@mail.ru>
2023-03-16 15:39:07 +0200

    Type User not type Album!!!

--- src/db/services/userServ.ts
+++ src/db/services/userServ.ts
@@ -3,7 +3,7 @@ import { eq } from "drizzle-orm/expressions";
 import { users } from "../schema/users";
 import { InferModel } from "drizzle-orm/mysql-core/table";
 
-export type Album = InferModel<typeof users>;
+export type Users = InferModel<typeof users>;
 
 export const getUser = async function (phone: string) {
     return await db.select().from(users).where(eq(users.phone, phone));
