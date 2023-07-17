8cbf8a757108950f9e2def9d493178709a8e544b
LemonardoD <90209095+LemonardoD@users.noreply.github.com>
2023-07-17 10:23:26 +0300

    Update index.js

--- lambdaframselfie/index.js
+++ lambdaframselfie/index.js
@@ -3,9 +3,9 @@ import mysql from "mysql2/promise";
 export const poolConnection = mysql.createPool({
     host: "127.0.0.1",
     port: 3306,
-    user: "root",
-    password: "david35221311972",
-    database: "framology",
+    user: "",
+    password: "",
+    database: "",
 });
 
 export async function handler(event) {
